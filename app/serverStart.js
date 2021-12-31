adbb22243d9953137400789255aa058a1bce16f2
Will <will_constable@msn.com>
2021-12-31 17:30:55 +0000

    tweak

--- app/serverStart.js
+++ app/serverStart.js
@@ -1,7 +1,7 @@
 const { PORT, NODE_ENV } = process.env
 
 module.exports = async (app) => {
-  // If not test start server
+  // Only start the server if not in test mode
   if (NODE_ENV !== 'test') {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
